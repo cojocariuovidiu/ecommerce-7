02c3898418356f73227e2e848f563aa6b5474e70
Rafael Cardoso <rafaelcardoso@Rafaels-MacBook-Pro.local>
2016-06-29 23:57:57 -0300

    modelagem do banco

--- model/db.js
+++ model/db.js
@@ -6,3 +6,49 @@
 
 // create folder data mongo
 // mkdir -p /data/db
+
+
+/*Usuario {
+	tipo: ,
+	nome: ,
+	email: ,
+	senha: ,
+	telefone: ,
+	celular: ,
+	nascimento: ,
+	endereco: {
+		logradouro:, 
+		numero:, 
+		bairro:, 
+		complemento:, 
+		cep:, 
+		cidade:, 
+		estado:
+	}
+}
+
+Banner:{
+	descricao:, 
+	imagem:, 
+	ordem:, 
+	status: 
+}
+
+Produto:{
+	nome:, 
+	descricao:, 
+	codigo:, 
+	preco_de:, 
+	preco_por:,
+	produto_filho: [] => Produto,
+	categoria:{
+		nome:, 
+		ordem:, 
+		status:
+		categoria_filho: [] => Categoria
+	},
+	imagem_principal:,
+	imagem_detalhe_1:,
+	imagem_detalhe_2:,
+	imagem_detalhe_3:
+}*/
